15c7229e3bd25ac7ec4d45fac9e6642838fcf9cc
DaFDE31 <adjocydanielson@gmail.com>
2023-12-04 15:15:44 +0000

    Map attem

---
+++ project.h
@@ -0,0 +1,16 @@
+typedef struct Key
+{
+    int key;
+};
+typedef struct Value
+{
+    int value;
+};
+typedef struct Map
+{
+    Key key;
+    Value value;
+    Value get(Key key){
+    }
+    
+};
