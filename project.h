5e2d5ec50acf8bf4f6a19b42e06972863c79446f
Sameer <sameer.patelde@gmail.com>
2023-12-12 17:25:29 +0000

    commit test

--- project.h
+++ project.h
@@ -2,10 +2,29 @@ typedef struct Key
 {
     int key;
 };
+
 typedef struct Value
 {
     int value;
 };
+
+//Struct for an entry in the page table
+typedef struct 
+{
+    unsigned char* frame;
+    int valid;
+}entry;
+///////////////////////////////////////
+
+void establish();// invalidate all entries
+
+void* update(int page); //grabs a value from the backing store and puts it in the page table
+
+unsigned char* search(int page);//Should be changed to return a value to the TLB
+
+
+// NOTE: COMMENTED OUT DUE TO ERRORS, FIX AND THEN UNCOMMENT
+/* 
 typedef struct Map
 {
     Key key;
@@ -14,3 +33,15 @@ typedef struct Map
     }
     
 };
+*/
+
+/**
+ * findAddress - Takes an argument of the line number to get the integer from, Outputs the integer held 
+ * within the target line number.
+*/
+int findAddress(int targetLineNum);
+
+/**
+* readFromBackingStore - Updates the page number and offset based on the inputted logical address
+*/
+void readFromBackingStore(int logical_address);
